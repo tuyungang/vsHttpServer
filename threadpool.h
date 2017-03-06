7a7b40f13d64a28369fa00e9e238d990b2fe9225
tuyungang <tyg362153368@126.com>
2017-03-06 19:24:23 +0800

    更改线程池的实现方式，按任务繁忙度实现线程池中线程数的增减，并与分配给当前空闲线程的任务对象绑定

--- threadpool.h
+++ threadpool.h
@@ -1,127 +1,155 @@
-#ifndef THREADPOOL_H
-#define THREADPOOL_H
+#ifndef _THREAD_POOL
+#define _THREAD_POOL
 
-#include <list>
-#include <cstdio>
-#include <exception>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <cassert>
+#include <sys/epoll.h>
+#include <vector>
+#include <algorithm>
 #include <pthread.h>
-#include "locker.h"
+#include "threadlock.h"
 
-template< typename T >
-class threadpool
+using namespace std;
+
+typedef enum {
+    THREAD_NONE = 0,
+    THREAD_READY = 1,
+    THREAD_RUNNING,
+    THREAD_BLOCKED,
+    THREAD_TERMINATED
+}ThreadState;
+
+class CThread
 {
-public:
-    threadpool( int thread_number = 8, int max_requests = 10000 );
-    ~threadpool();
-    bool append( T* request );
-    pthread_t* get_threads(){ return m_threads; }
-
-private:
-    static void* worker( void* arg );
-    void run();
-
-private:
-    int m_thread_number;
-    int m_max_requests;
-    pthread_t* m_threads;
-    std::list< T* > m_workqueue;
-    locker m_queuelocker;
-    sem m_queuestat;
-    bool m_stop;
+    private:
+        int m_ErrCode;
+        CSemaphore m_ThreadSemaphore;
+        //unsigned long m_ThreadID;
+        pthread_t *m_ThreadID;
+        bool m_Detach;
+        bool m_CreateSuspended;
+        char *m_ThreadName;
+        ThreadState m_ThreadState;
+    protected:
+        void SetErrcode(int errcode){m_ErrCode = errcode;}
+        static void *ThreadFunction(void*);
+    public:
+        CThread();
+        CThread(bool createsuspended, bool detach);
+        virtual ~CThread();
+        virtual void Run(void)=0;
+        void SetThreadState(ThreadState state){m_ThreadState = state;}
+        bool Terminate(void);
+        bool Start(void);
+        //void Exit(void);
+        //bool Wakeup(void);
+        ThreadState GetThreadState(void){return m_ThreadState;}
+        //int GetLastError(void){return m_ErrCode;}
+        void SetThreadName(char *thrname){strcpy(m_ThreadName, thrname);}
+        char *GetThreadName(void){return m_ThreadName;}
+        //int GetThreadID(void){return m_ThreadID;}
+        pthread_t* GetThreadID(void){return m_ThreadID;}
+        //bool SetPriorit(int priority);
+        //int GetPriority(void);
+        //int GetConcurrency(void);
+        //void SetConcurrency(int num);
+        bool Detach(void);
+        //bool Join(void){pthread_join(*m_ThreadID,NULL);}
+        bool Join(void);
+        //bool Yield(void);
+        //int Self(void);
 };
 
-template< typename T >
-threadpool< T >::threadpool( int thread_number, int max_requests ) : 
-        m_thread_number( thread_number ), m_max_requests( max_requests ), m_stop( false ), m_threads( NULL )
-{
-    if( ( thread_number <= 0 ) || ( max_requests <= 0 ) )
-    {
-        throw std::exception();
-    }
+class CJob;
+class CThreadPool;
 
-    m_threads = new pthread_t[ m_thread_number ];
-    if( ! m_threads )
-    {
-        throw std::exception();
-    }
+class CWorkerThread:public CThread 
+{ 
+    private: 
+        CThreadPool*  m_ThreadPool; 
+        CJob*    m_Job; 
+        void*    m_JobData; 
 
-    for ( int i = 0; i < thread_number; ++i )
-    {
-        printf( "create the %dth thread\n", i );
-        if( pthread_create( m_threads + i, NULL, worker, this ) != 0 )
-        {
-            delete [] m_threads;
-            throw std::exception();
-        }
-        /*
-        if( pthread_detach( m_threads[i] ) )
-        {
-            delete [] m_threads;
-            throw std::exception();
-        }
-        */
-    }
-}
+        CThreadMutex m_VarMutex; 
+        bool      m_IsEnd; 
+    protected: 
+    public: 
+        CCondition   m_JobCond; 
+        CThreadMutex m_WorkMutex; 
+    public:
+        CWorkerThread(); 
+        virtual ~CWorkerThread(); 
+    public:
+        void Run(); 
+        void SetJob(CJob* job,void* jobdata); 
+        CJob *GetJob(void){return m_Job;} 
+        void SetThreadPool(CThreadPool* thrpool); 
+        CThreadPool* GetThreadPool(void){return m_ThreadPool;} 
+};
 
-template< typename T >
-threadpool< T >::~threadpool()
-{
-    delete [] m_threads;
-    m_stop = true;
-}
 
-template< typename T >
-bool threadpool< T >::append( T* request )
-{
-    m_queuelocker.lock();
-    if ( m_workqueue.size() > m_max_requests )
-    {
-        m_queuelocker.unlock();
-        return false;
-    }
-    m_workqueue.push_back( request );
-    m_queuelocker.unlock();
-    m_queuestat.post();
-    return true;
-}
+class CThreadPool 
+{ 
+    friend class CWorkerThread; 
+    private: 
+        unsigned int m_MaxNum;   
+        unsigned int m_AvailLow;  
+        unsigned int m_AvailHigh;    
+        unsigned int m_AvailNum;  
+        unsigned int m_InitNum;  
+    protected: 
+        CWorkerThread* GetIdleThread(void); 
 
-template< typename T >
-void* threadpool< T >::worker( void* arg )
-{
-    int err = pthread_detach( pthread_self() );
-    threadpool* pool = ( threadpool* )arg;
-    if( err != 0 )
-    {
-        delete [] (pool->get_threads());
-        throw std::exception();
-    }
+        void    AppendToIdleList(CWorkerThread* jobthread); 
+        void    MoveToBusyList(CWorkerThread* idlethread); 
+        void    MoveToIdleList(CWorkerThread* busythread); 
 
-    //threadpool* pool = ( threadpool* )arg;
-    pool->run();
-    return pool;
-}
+        void    DeleteIdleThread(int num); 
+        void    CreateIdleThread(int num); 
+    public: 
+        CThreadMutex m_BusyMutex;     
+        CThreadMutex m_IdleMutex;     
+        CThreadMutex m_JobMutex;  
+        CThreadMutex m_VarMutex; 
 
-template< typename T >
-void threadpool< T >::run()
-{
-    while ( ! m_stop )
-    {
-        m_queuestat.wait();
-        m_queuelocker.lock();
-        if ( m_workqueue.empty() )
-        {
-            m_queuelocker.unlock();
-            continue;
-        }
-        T* request = m_workqueue.front();
-        m_workqueue.pop_front();
-        m_queuelocker.unlock();
-        if ( ! request )
-        {
-            continue;
-        }
-        request->process();
-    }
-}
+        CCondition       m_BusyCond;  
+        CCondition       m_IdleCond;  
+        CCondition       m_IdleJobCond;   
+        CCondition       m_MaxNumCond; 
+
+        vector<CWorkerThread*>   m_ThreadList; 
+        vector<CWorkerThread*>   m_BusyList;      
+        vector<CWorkerThread*>   m_IdleList;  
+        vector<CJob*> m_JobList;
+
+        CThreadPool(); 
+        CThreadPool(int initnum); 
+        virtual ~CThreadPool(); 
+
+        void    SetMaxNum(int maxnum){m_MaxNum = maxnum;} 
+        int     GetMaxNum(void){return m_MaxNum;} 
+        void    SetAvailLowNum(int minnum){m_AvailLow = minnum;} 
+        int     GetAvailLowNum(void){return m_AvailLow;} 
+        void    SetAvailHighNum(int highnum){m_AvailHigh = highnum;} 
+        int     GetAvailHighNum(void){return m_AvailHigh;} 
+        int     GetActualAvailNum(void){return m_AvailNum;} 
+        int     GetAllNum(void){return m_ThreadList.size();} 
+        int     GetBusyNum(void){return m_BusyList.size();} 
+        void    SetInitNum(int initnum){m_InitNum = initnum;} 
+        int     GetInitNum(void){return m_InitNum;} 
+
+        void    TerminateAll(void); 
+        void    Run(CJob* job,void* jobdata); 
+};
 
 #endif
