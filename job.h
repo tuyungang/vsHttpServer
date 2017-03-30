e0a464783fd915ebbf4695ca8f4ac0f19a342aa6
tuyungang <tyg362153368@126.com>
2017-03-30 08:42:39 +0800

    结点信息类的父类，有几处小的改动

--- job.h
+++ job.h
@@ -16,11 +16,11 @@
 
 #include <signal.h>
 #include <sys/types.h>
+//#include <assert.h>
 #include <sys/stat.h>
 #include <pthread.h>
 #include <sys/mman.h>
 #include <stdarg.h>
-//#include "locker.h"
 
 using namespace std;
 
@@ -28,22 +28,22 @@ class CThread;
 class CJob
 {
 private: 
-        int      m_JobNo;         
-        char*    m_JobName;       
-        CThread  *m_pWorkThread;     
+        int m_JobNo;         
+        char *m_JobName;       
+        CThread *m_pWorkThread;     
     public: 
         CJob( void ); 
         virtual ~CJob(); 
         
-        int      GetJobNo(void) const { return m_JobNo; } 
-        void     SetJobNo(int jobno){ m_JobNo = jobno;} 
-        char*    GetJobName(void) const { return m_JobName; } 
-        void     SetJobName(char* jobname); 
+        int GetJobNo(void) const { return m_JobNo; } 
+        void SetJobNo(int jobno){ m_JobNo = jobno;} 
+        char *GetJobName(void) const { return m_JobName; } 
+        void SetJobName(char* jobname); 
         CThread *GetWorkThread(void){ return m_pWorkThread; } 
-        void     SetWorkThread ( CThread *pWorkThread ){ 
+        void SetWorkThread ( CThread *pWorkThread ){ 
             m_pWorkThread = pWorkThread; 
         } 
-        virtual void Run ( void *ptr ) = 0; 
+        virtual void Run( void *ptr ) = 0; 
 
 public:
     static const int FILENAME_LEN = 200;
@@ -51,14 +51,14 @@ public:
     static const int WRITE_BUFFER_SIZE = 1024;
     enum METHOD { GET = 0, POST, HEAD, PUT, DELETE, TRACE, OPTIONS, CONNECT, PATCH };
     enum CHECK_STATE { CHECK_STATE_REQUESTLINE = 0, CHECK_STATE_HEADER, CHECK_STATE_CONTENT };
-    enum HTTP_CODE { NO_REQUEST, GET_REQUEST, HEAD_REQUEST, POST_REQUEST, PUT_REQUEST, DELETE_REQUEST, TRACE_REQUEST, OPTIONS_REQUEST, CONNECT_REQUEST, PATCH_REQUEST/*add by tu--处理方法head请求*/, BAD_REQUEST, NO_RESOURCE, FORBIDDEN_REQUEST, FILE_REQUEST, INTERNAL_ERROR, CLOSED_CONNECTION };
+    enum HTTP_CODE { NO_REQUEST, GET_REQUEST, HEAD_REQUEST, POST_REQUEST, PUT_REQUEST, DELETE_REQUEST, TRACE_REQUEST, OPTIONS_REQUEST, CONNECT_REQUEST, PATCH_REQUEST, BAD_REQUEST, NO_RESOURCE, FORBIDDEN_REQUEST, FILE_REQUEST, INTERNAL_ERROR, CLOSED_CONNECTION };
     enum LINE_STATUS { LINE_OK = 0, LINE_BAD, LINE_OPEN };
 
 public:
     void init( int sockfd, const sockaddr_in& addr );
     void close_conn( bool real_close = true );
     void process();
-    bool read();
+    //bool read();
     bool write();
 
 private:
@@ -83,16 +83,20 @@ private:
     bool add_blank_line();
 
 public:
-    static int m_epollfd;
-    static int m_user_count;
+    //static int m_epollfd;
+    //static int m_user_count;  //modified by tu
 
-private:
-    int m_sockfd;
-    sockaddr_in m_address;
-
-    char m_read_buf[ READ_BUFFER_SIZE ];
+public:
+    char m_read_buf[READ_BUFFER_SIZE];
     int m_read_idx;
     int m_checked_idx;
+private:
+    //int m_sockfd;
+    sockaddr_in m_address;
+
+    //char m_read_buf[ READ_BUFFER_SIZE ];
+    //int m_read_idx;
+    //int m_checked_idx;
     int m_start_line;
     char m_write_buf[ WRITE_BUFFER_SIZE ];
     int m_write_idx;
@@ -110,7 +114,7 @@ private:
 
     char* m_file_address;
     struct stat m_file_stat;
-    struct iovec m_iv[2];
+    //struct iovec m_iv[2];
     int m_iv_count;
 };
 
