170bf31946fc2ed29f54a7a3bc91a5bc76cce6ba
tuyungang <tyg362153368@126.com>
2017-03-30 08:53:05 +0800

    改动了入口函数部分，增加一个静态变量来对对象计数

--- threadmanage.cpp
+++ threadmanage.cpp
@@ -1,36 +1,56 @@
 #include "threadmanage.h"
 #include "threadlock.h"
+#include "job.h"
+#include "threadpool.h"
 
 CThreadManage* CThreadManage::pInstance = NULL;
+int CThreadManage::m_numObjects = 0;
 
 CThreadManage::CThreadManage(){ 
+    ++m_numObjects;
     m_NumOfThread = 10; 
-    m_Pool = new CThreadPool(m_NumOfThread); 
+    m_WorkerPool = new CThreadPool(); 
+   // m_WorkerPool = new CThreadPool(m_NumOfThread); 
 } 
 
+/*
 CThreadManage::CThreadManage(int num){ 
+    ++m_numObjects;
     m_NumOfThread = num; 
     m_Pool = new CThreadPool(m_NumOfThread); 
 } 
+*/
+
+CThreadManage::CThreadManage(int num, void *arg)
+{
+    ++m_numObjects;
+    m_NumOfThread = num;
+    m_Pool = new CThreadPool(m_NumOfThread,arg);
+}
 
 CThreadManage::~CThreadManage(){ 
-     if(NULL != m_Pool) 
-     delete m_Pool; 
+     if((NULL != m_Pool) &&(NULL != m_WorkerPool)){ 
+        delete m_Pool; 
+        delete m_WorkerPool;
+     }
+     --m_numObjects; 
  } 
 
-CThreadManage* CThreadManage::init(int thread_num)
+CThreadManage* CThreadManage::init(int thread_num, void *arg)
 {
     CThreadMutex m_lock;
-    if(pInstance == NULL){
+    if(m_numObjects < MAX_OBJECT){
         m_lock.lock();
-        if(pInstance == NULL){
+        if(m_numObjects < MAX_OBJECT){
             if(thread_num == 0){
                 pInstance = new CThreadManage();
             }else {
-                pInstance = new CThreadManage(thread_num);
+                pInstance = new CThreadManage(thread_num, arg);
             }
         }
         m_lock.unlock();
+    }else{
+        throw MyException();
     }
     return pInstance;
 }
@@ -42,7 +62,7 @@ void CThreadManage::SetParallelNum(int num)
 
 void CThreadManage::Run(CJob* job,void* jobdata)
 { 
-    m_Pool->Run(job,jobdata); 
+    m_WorkerPool->Run(job,jobdata); 
 } 
 
 void CThreadManage::TerminateAll(void)
