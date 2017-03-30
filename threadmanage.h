3534801bec74bc2bedc04c5da2ffb8a37b9bcf65
tuyungang <tyg362153368@126.com>
2017-03-30 08:49:38 +0800

    改成多例模式，只能产生两个线程池管理对象，一个负责libevent线程池的管理，一个用于处理具体任务的线程池

--- threadmanage.h
+++ threadmanage.h
@@ -15,43 +15,78 @@
 #include <fcntl.h>
 #include <cassert>
 #include <sys/epoll.h>
-#include "job.h"
-#include "threadpool.h"
+#include <exception>
 
 using namespace std;
 
+class CThreadPool;
+class CJob;
+
 class CThreadManage
 {
+    public:
+        enum {MAX_OBJECT = 2000};
     private:
         CThreadManage();
-        CThreadManage(int num);
-        ~CThreadManage();
+        //CThreadManage(int num);
+        CThreadManage(int num,void *arg); 
+        //~CThreadManage();
         //virtual ~CThreadManage();
         //CThreadManage(const CThreadManage &);  
         //CThreadManage & operator = (const CThreadManage &);
     private:
         CThreadPool *m_Pool;
+        CThreadPool *m_WorkerPool;
         int m_NumOfThread;
         static CThreadManage *pInstance;
+        static int m_numObjects;
     protected:
     public:
-        static CThreadManage *init(int thread_num);
+        ~CThreadManage(); 
+        class MyException : public exception{
+            public:
+                MyException():exception(){}
+                const char *what()const throw(){return "ERROR! over two objects is not allowed!";}
+        };
+        static CThreadManage *init(int thread_num, void *arg);
         void SetParallelNum(int num);
         //CThreadManage();
         //CThreadManage(int num);
         //virtual ~CThreadManage();
         void Run(CJob *job, void *jobdata);
+        CThreadPool *GetThreadPool(void){return m_Pool;}
+        CThreadPool *GetWorkerThreadPool(void){return m_WorkerPool;}
         void TerminateAll(void);
-        class freeInstance
+        class FreeInstance
         {
             public:
-                ~freeInstance(){
+                ~FreeInstance(){
                     if(CThreadManage::pInstance){
                         delete CThreadManage::pInstance;
                     }
                 }
         };
-        static freeInstance _instance;
+        static FreeInstance _instance;
 };
+
+/*
+class CMainThreadManage : public CThreadManage
+{
+    friend class CThreadManage;
+    private:
+        CMainThreadManage(){}
+    public:
+    
+};
+
+class CWorkerThreadManage : public CThreadManage
+{
+    friend class CThreadManage;
+    private:
+        CWorkerThreadManage(){}
+    public:
+
+};
+*/
 
 #endif
