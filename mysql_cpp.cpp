99b8404726bd2bf1cb5b713680c256010d632094
tuyungang <tyg362153368@126.com>
2017-05-08 00:46:46 +0800

    数据库语句执行类实现

--- mysql_cpp.cpp
+++ mysql_cpp.cpp
@@ -1,38 +1,33 @@
 #include "mysql_cpp.h"
 
-MysqlCPP* MysqlCPP::_instance = NULL;
-
-
+/*
 MysqlCPP::MysqlCPP()
 {
-    if(mysql_init(&_mysql) == NULL){
+    if(mysql_init(mysql) == NULL){
         throw std::exception();
     }
-    //mysql_options();
-
 }
-
-MysqlCPP::~MysqlCPP()
+*/
+MysqlCPP::MysqlCPP(MYSQL *mSQL)
 {
-
+    mysql = mSQL;
 }
 
+/*
 void MysqlCPP::Connect_Mysql()
 {
-    //if(!mysql_real_connect(&_mysql,"localhost","root","123456","test",0,NULL,CLIENT_FOUND_ROWS)){
-    if(!mysql_real_connect(&_mysql,m_ConnInfo.host,m_ConnInfo.user,m_ConnInfo.password,m_ConnInfo.database,m_ConnInfo.port,NULL,CLIENT_FOUND_ROWS)){
-        fprintf(stderr, "Failed to connect to database: Error: %s\n", mysql_error(&_mysql));
+    if(!mysql_real_connect(mysql,m_ConnInfo.host,m_ConnInfo.user,m_ConnInfo.password,m_ConnInfo.database,m_ConnInfo.port,NULL,CLIENT_FOUND_ROWS)){
+        fprintf(stderr, "Failed to connect to database: Error: %s\n", mysql_error(mysql));
         throw std::exception();
     }
     cout << "connect mysql server sucessfully" << endl;
-
 }
 
 void MysqlCPP::Close_Mysql()
 {
-    mysql_close(&_mysql);
-
+    mysql_close(mysql);
 }
+*/
 
 void MysqlCPP::Mysql_HandleSQL(const char *query)
 {
@@ -40,25 +35,27 @@ void MysqlCPP::Mysql_HandleSQL(const char *query)
     MYSQL_FIELD *field;
     unsigned int num_fields;
     unsigned int num_rows;
-    //int ret = mysql_real_query(&_mysql,query,strlen(query)); 
-    int ret = mysql_query(&_mysql,query); 
+    //int ret = mysql_real_query(mysql,query,strlen(query)); 
+    int ret = mysql_query(mysql,query); 
     if(ret != 0){
-        if(!mysql_ping(&_mysql)){
-            if(!mysql_query(&_mysql,query)){
+        if(!mysql_ping(mysql)){
+            if(!mysql_query(mysql,query)){
             }else
                 throw std::exception();
         }else{
+            /*
             try{
                 Connect_Mysql();
             }catch(std::exception &e){
                 cout << e.what() << endl;
             }
             throw std::exception();
+            */
         }
     }
 
     do{
-        m_Result = mysql_store_result(&_mysql);
+        m_Result = mysql_store_result(mysql);
         if(m_Result){
             num_fields = mysql_num_fields(m_Result);
             field = mysql_fetch_fields(m_Result);
@@ -71,28 +68,29 @@ void MysqlCPP::Mysql_HandleSQL(const char *query)
             mysql_free_result(m_Result);
 
         }else{
-            if(mysql_field_count(&_mysql) == 0){
+            if(mysql_field_count(mysql) == 0){
                 cout << "not select sql" << endl;
-                num_rows = mysql_affected_rows(&_mysql);
+                num_rows = mysql_affected_rows(mysql);
             }else{
-                fprintf(stderr, "Error: %s\n",mysql_error(&_mysql));
+                fprintf(stderr, "Error: %s\n",mysql_error(mysql));
             }
         }
-    }while(!mysql_next_result(&_mysql));
+    }while(!mysql_next_result(mysql));
 
 }
 
+/*
 void MysqlCPP::Mysql_SelectSQL(const char *query)
 {
     MYSQL_ROW row;
     MYSQL_FIELD *field;
     unsigned int num_fields;
     unsigned int num_rows;
-    //int ret = mysql_real_query(&_mysql,query,strlen(query)); 
-    int ret = mysql_query(&_mysql,query); 
+    //int ret = mysql_real_query(mysql,query,strlen(query)); 
+    int ret = mysql_query(mysql,query); 
     if(ret != 0){
-        if(!mysql_ping(&_mysql)){
-            if(!mysql_query(&_mysql,query)){
+        if(!mysql_ping(mysql)){
+            if(!mysql_query(mysql,query)){
             }else
                 throw std::exception();
         }else{
@@ -106,7 +104,7 @@ void MysqlCPP::Mysql_SelectSQL(const char *query)
     }
 
     do{
-        m_Result = mysql_store_result(&_mysql);
+        m_Result = mysql_store_result(mysql);
         if(m_Result){
             num_fields = mysql_num_fields(m_Result);
             field = mysql_fetch_fields(m_Result);
@@ -121,14 +119,14 @@ void MysqlCPP::Mysql_SelectSQL(const char *query)
 
 
         }else{
-            if(mysql_field_count(&_mysql) == 0){
+            if(mysql_field_count(mysql) == 0){
                 cout << "not select sql" << endl;
-                num_rows = mysql_affected_rows(&_mysql);
+                num_rows = mysql_affected_rows(mysql);
             }else{
-                fprintf(stderr, "Error: %s\n",mysql_error(&_mysql));
+                fprintf(stderr, "Error: %s\n",mysql_error(mysql));
             }
         }
-    }while(!mysql_next_result(&_mysql));
+    }while(!mysql_next_result(mysql));
 
 }
 
@@ -138,11 +136,11 @@ void MysqlCPP::Mysql_InsertSQL(const char *query)
     MYSQL_FIELD *field;
     unsigned int num_fields;
     unsigned int num_rows;
-    //int ret = mysql_real_query(&_mysql,query,strlen(query)); 
-    int ret = mysql_query(&_mysql,query); 
+    //int ret = mysql_real_query(mysql,query,strlen(query)); 
+    int ret = mysql_query(mysql,query); 
     if(ret != 0){
-        if(!mysql_ping(&_mysql)){
-            if(!mysql_query(&_mysql,query)){
+        if(!mysql_ping(mysql)){
+            if(!mysql_query(mysql,query)){
             }else
                 throw std::exception();
         }else{
@@ -151,7 +149,7 @@ void MysqlCPP::Mysql_InsertSQL(const char *query)
     }
 
     do{
-        m_Result = mysql_store_result(&_mysql);
+        m_Result = mysql_store_result(mysql);
         if(m_Result){
             num_fields = mysql_num_fields(m_Result);
             field = mysql_fetch_fields(m_Result);
@@ -165,14 +163,14 @@ void MysqlCPP::Mysql_InsertSQL(const char *query)
 
 
         }else{
-            if(mysql_field_count(&_mysql) == 0){
+            if(mysql_field_count(mysql) == 0){
                 cout << "insert sql" << endl;
-                num_rows = mysql_affected_rows(&_mysql);
+                num_rows = mysql_affected_rows(mysql);
             }else{
-                fprintf(stderr, "Error: %s\n",mysql_error(&_mysql));
+                fprintf(stderr, "Error: %s\n",mysql_error(mysql));
             }
         }
-    }while(!mysql_next_result(&_mysql));
+    }while(!mysql_next_result(mysql));
 
 }
 
@@ -182,11 +180,11 @@ void MysqlCPP::Mysql_UpdateSQL(const char *query)
     MYSQL_FIELD *field;
     unsigned int num_fields;
     unsigned int num_rows;
-    //int ret = mysql_real_query(&_mysql,query,strlen(query)); 
-    int ret = mysql_query(&_mysql,query); 
+    //int ret = mysql_real_query(mysql,query,strlen(query)); 
+    int ret = mysql_query(mysql,query); 
     if(ret != 0){
-        if(!mysql_ping(&_mysql)){
-            if(!mysql_query(&_mysql,query)){
+        if(!mysql_ping(mysql)){
+            if(!mysql_query(mysql,query)){
             }else
                 throw std::exception();
         }else{
@@ -195,7 +193,7 @@ void MysqlCPP::Mysql_UpdateSQL(const char *query)
     }
 
     do{
-        m_Result = mysql_store_result(&_mysql);
+        m_Result = mysql_store_result(mysql);
         if(m_Result){
             num_fields = mysql_num_fields(m_Result);
             field = mysql_fetch_fields(m_Result);
@@ -209,14 +207,14 @@ void MysqlCPP::Mysql_UpdateSQL(const char *query)
 
 
         }else{
-            if(mysql_field_count(&_mysql) == 0){
+            if(mysql_field_count(mysql) == 0){
                 cout << "update sql" << endl;
-                num_rows = mysql_affected_rows(&_mysql);
+                num_rows = mysql_affected_rows(mysql);
             }else{
-                fprintf(stderr, "Error: %s\n",mysql_error(&_mysql));
+                fprintf(stderr, "Error: %s\n",mysql_error(mysql));
             }
         }
-    }while(!mysql_next_result(&_mysql));
+    }while(!mysql_next_result(mysql));
 
 }
 
@@ -226,11 +224,11 @@ void MysqlCPP::Mysql_DeleteSQL(const char *query)
     MYSQL_FIELD *field;
     unsigned int num_fields;
     unsigned int num_rows;
-    //int ret = mysql_real_query(&_mysql,query,strlen(query)); 
-    int ret = mysql_query(&_mysql,query); 
+    //int ret = mysql_real_query(mysql,query,strlen(query)); 
+    int ret = mysql_query(mysql,query); 
     if(ret != 0){
-        if(!mysql_ping(&_mysql)){
-            if(!mysql_query(&_mysql,query)){
+        if(!mysql_ping(mysql)){
+            if(!mysql_query(mysql,query)){
             }else
                 throw std::exception();
         }else{
@@ -239,7 +237,7 @@ void MysqlCPP::Mysql_DeleteSQL(const char *query)
     }
 
     do{
-        m_Result = mysql_store_result(&_mysql);
+        m_Result = mysql_store_result(mysql);
         if(m_Result){
             num_fields = mysql_num_fields(m_Result);
             field = mysql_fetch_fields(m_Result);
@@ -253,17 +251,19 @@ void MysqlCPP::Mysql_DeleteSQL(const char *query)
 
 
         }else{
-            if(mysql_field_count(&_mysql) == 0){
+            if(mysql_field_count(mysql) == 0){
                 cout << "delete sql" << endl;
-                num_rows = mysql_affected_rows(&_mysql);
+                num_rows = mysql_affected_rows(mysql);
             }else{
-                fprintf(stderr, "Error: %s\n",mysql_error(&_mysql));
+                fprintf(stderr, "Error: %s\n",mysql_error(mysql));
             }
         }
-    }while(!mysql_next_result(&_mysql));
+    }while(!mysql_next_result(mysql));
 
 }
+*/
 
+/*
 void MysqlCPP::SetMySqlConnInfo( const char *server, const char *username, const char *password, const char *database,int port)
 {
     strcpy(m_ConnInfo.host, server);
@@ -272,6 +272,7 @@ void MysqlCPP::SetMySqlConnInfo( const char *server, const char *username, const
     strcpy(m_ConnInfo.database, database);
     m_ConnInfo.port = port;
 }
+*/
 
 void MysqlCPP::Mysql_ErrorInfo()
 {
